5ca558736bac9d5e54d52c79e56bf0cf50e90734
Nicolas Lépinay <nicolas@nosoft.io>
2023-12-19 09:56:05 +0100

    Refresh token expiration

--- api/utils/tokenUtils.js
+++ api/utils/tokenUtils.js
@@ -5,5 +5,5 @@ exports.generateToken = (user) => {
 };
 
 exports.generateRefreshToken = (user) => {
-    return jwt.sign({ id: user._id, roles: user.roles }, process.env.JWT_REFRESH_SECRET, { expiresIn: '1d' });
+    return jwt.sign({ id: user._id, roles: user.roles }, process.env.JWT_REFRESH_SECRET, { expiresIn: '2h' });
 };
